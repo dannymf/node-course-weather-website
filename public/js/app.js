c14b8eea50e2af903f2c1af82818ebb06049616e
System Administrator <root@daniels-mbp-3.myfiosgateway.com>
2021-06-03 23:15:20 -0400

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Javascript client side loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
